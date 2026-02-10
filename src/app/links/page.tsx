c2a6775fe5eeb76f3c20218d5ce29a4597d7c71c
Advansoftware <brunoantunes94@hotmail.com>
2026-02-09 23:34:34 -0300

    ajustando teste gratis

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -14,7 +14,7 @@ import AdvanLogo from '@/components/AdvanLogo';
 export default function Home() {
   const links = [
     {
-      title: 'Teste Premium Grátis',
+      title: 'Teste Grátis',
       subtitle: 'Solicite seu teste agora mesmo',
       icon: <PlayCircleOutlineIcon />,
       href: '/testegratis', // Goes to our internal route which redirects to WA
